b6c5d6442d7a7a01a8f20ceea030e5f8ffaa8fed
Miles Hollingsworth <mhmiles@me.com>
2016-06-29 12:55:52 -0700

    Initial Commit

---
+++ ReactiveReachability/ReactiveReachability.h
@@ -0,0 +1,19 @@
+//
+//  ReactiveReachability.h
+//  ReactiveReachability
+//
+//  Created by Miles Hollingsworth on 6/29/16.
+//  Copyright © 2016 Miles Hollingsworth. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ReactiveReachability.
+FOUNDATION_EXPORT double ReactiveReachabilityVersionNumber;
+
+//! Project version string for ReactiveReachability.
+FOUNDATION_EXPORT const unsigned char ReactiveReachabilityVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ReactiveReachability/PublicHeader.h>
+
+
