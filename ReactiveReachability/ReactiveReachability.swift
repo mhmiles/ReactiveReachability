6a86f04c10bd161d745c31c1d7f68e6b265cae27
Miles Hollingsworth <mhmiles@me.com>
2018-03-26 12:17:53 -0700

    renamed singleton

--- ReactiveReachability/ReactiveReachability.swift
+++ ReactiveReachability/ReactiveReachability.swift
@@ -11,7 +11,7 @@ import enum Result.NoError
 import Reachability
 
 open class ReactiveReachability {
-    public static let sharedReachability = ReactiveReachability()
+    public static let shared = ReactiveReachability()
     
     public private(set) lazy var isReachableViaWiFi: Property<Bool> = {
         guard let reachability = self.reachability else {
